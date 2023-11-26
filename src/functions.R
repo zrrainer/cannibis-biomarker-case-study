52c77d0f8db3e241b2ae7d0e4149fdffce6703b9
Rainer Zhang <ziz110@ucsd.edu>
2023-11-26 05:34:50 +0000

    2023.22.25

--- src/functions.R
+++ src/functions.R
@@ -10,25 +10,25 @@ drop_dups <- function(dataset){
 
 
 
-#plot value of compound across time
+#plot value of compound across time, coloured by treatment
 #signature:
   #map(compounds_WB/compounds_BR/compounds_OF, ~ compound_scatterplot_group( 
   #dataset=BR/OF/BR, 
   #compound=.x, 
   #timepoints=timepoints_WB/timepoints_BR/timepoints_OF))
-compound_scatterplot_group <- function(dataset, compound, timepoints){
+compound_scatterplot_group_by_treatment <- function(dataset, compound, timepoints){
   if(max(dataset[,compound],na.rm=TRUE)==0){
     print(
       dataset |> 
         filter(!is.na(time_from_start)) |>
         ggplot(aes_string(x="time_from_start", 
                           y=compound,
-                          color="group")) + 
+                          color="treatment")) + 
         geom_point() +
         geom_vline(data=timepoints, aes(xintercept=as.numeric(stop)), 
                    linetype="dashed", 
                    color="gray28") +
-        scale_color_manual(values=c("#19831C", "#A27FC9")) +
+        scale_color_manual(values=c("#19831C", "#A27FC9","#A27FC9")) +
         scale_y_continuous(limits=c(0,3)) +
         theme_classic() +
         theme(legend.position="bottom",
@@ -41,12 +41,61 @@ compound_scatterplot_group <- function(dataset, compound, timepoints){
           filter(!is.na(time_from_start)) |>
           ggplot(aes_string(x="time_from_start", 
                             y=compound,
-                            color="group")) + 
+                            color="treatment")) + 
           geom_point() +
           geom_vline(data=timepoints, aes(xintercept=as.numeric(stop)), 
                      linetype="dashed", 
                      color="gray28")  +
-          scale_color_manual(values=c("#19831C", "#A27FC9")) +
+          scale_color_manual(values=c("#19831C", "#A27FC9","#A27FC9")) +
+          theme_classic() +
+          theme(legend.position="bottom",
+                legend.title=element_blank()) +
+          labs(x='Time From Start (min)',
+               y=gsub('GLUC', 'gluc', gsub("_", "-", toupper(compound))))
+      )
+    }
+}
+
+
+#plot value of compound across time colour by treatment, log transformed
+#signature:
+#map(compounds_WB/compounds_BR/compounds_OF, ~ compound_scatterplot_group( 
+#dataset=BR/OF/BR, 
+#compound=.x, 
+#timepoints=timepoints_WB/timepoints_BR/timepoints_OF))
+compound_scatterplot_group_by_treatment_log <- function(dataset, compound, timepoints){
+  if(max(dataset[,compound],na.rm=TRUE)==0){
+    print(
+      dataset |> 
+        filter(!is.na(time_from_start)) |>
+        mutate(log_transformed_compound = log(get(compound)))|>
+        ggplot(aes_string(x="time_from_start", 
+                          y="log_transformed_compound",
+                          color="treatment")) + 
+        geom_point() +
+        geom_vline(data=timepoints, aes(xintercept=as.numeric(stop)), 
+                   linetype="dashed", 
+                   color="gray28") +
+        scale_color_manual(values=c("#19831C", "#A27FC9","#A27FC9")) +
+        scale_y_continuous(limits=c(0,3)) +
+        theme_classic() +
+        theme(legend.position="bottom",
+              legend.title=element_blank()) +
+        labs(x='Time From Start (min)',
+             y=gsub('GLUC', 'gluc',gsub("_", "-", toupper(compound))))
+    )}else{
+      print(
+        dataset |> 
+          filter(!is.na(time_from_start)) |>
+          mutate(log_transformed_compound = log(get(compound)))|>
+          ggplot(aes_string(x="time_from_start", 
+                            y="log_transformed_compound",
+                            color="treatment")) + 
+          geom_point() +
+          geom_vline(data=timepoints, aes(xintercept=as.numeric(stop)), 
+                     linetype="dashed", 
+                     color="gray28")  +
+          scale_color_manual(values=c("#19831C", "#A27FC9","#A27FC9")) +
           theme_classic() +
           theme(legend.position="bottom",
                 legend.title=element_blank()) +
